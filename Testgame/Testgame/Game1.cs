f42eca6bd4c27cf53134f3ffa35b119722c4f913
Ben Stallworth <benstallworth@gmail.com>
2012-06-18 17:22:42 -0400

    All the cards

--- Testgame/Testgame/Game1.cs
+++ Testgame/Testgame/Game1.cs
@@ -72,21 +72,61 @@ namespace Testgame
             gameplay = new Screen(background);
 
             // TODO: use this.Content to load your game content here
-            
-            cards = new Card[13];
-            cards[0] = new Card(0, this.Content.Load<Texture2D>("AceHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[1] = new Card(1, this.Content.Load<Texture2D>("KingHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[2] = new Card(2, this.Content.Load<Texture2D>("TwoHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[3] = new Card(3, this.Content.Load<Texture2D>("ThreeHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[4] = new Card(4, this.Content.Load<Texture2D>("FourHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[5] = new Card(5, this.Content.Load<Texture2D>("FiveHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[6] = new Card(6, this.Content.Load<Texture2D>("SixHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[7] = new Card(7, this.Content.Load<Texture2D>("SevenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[8] = new Card(8, this.Content.Load<Texture2D>("EightHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[9] = new Card(9, this.Content.Load<Texture2D>("NineHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[10] = new Card(10, this.Content.Load<Texture2D>("TenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[11] = new Card(11, this.Content.Load<Texture2D>("JackHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
-            cards[12]= new Card(12, this.Content.Load<Texture2D>("QueenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            #region Create cards[]
+            cards = new Card[52];
+            cards[0] = new Card(0, this.Content.Load<Texture2D>("AceClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[1] = new Card(1, this.Content.Load<Texture2D>("TwoClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[2] = new Card(2, this.Content.Load<Texture2D>("ThreeClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[3] = new Card(3, this.Content.Load<Texture2D>("FourClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[4] = new Card(4, this.Content.Load<Texture2D>("FiveClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[5] = new Card(5, this.Content.Load<Texture2D>("SixClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[6] = new Card(6, this.Content.Load<Texture2D>("SevenClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[7] = new Card(7, this.Content.Load<Texture2D>("EightClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[8] = new Card(8, this.Content.Load<Texture2D>("NineClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[9] = new Card(9, this.Content.Load<Texture2D>("TenClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[10] = new Card(10, this.Content.Load<Texture2D>("JackClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[11] = new Card(11, this.Content.Load<Texture2D>("QueenClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[12] = new Card(12, this.Content.Load<Texture2D>("KingClubs"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[13] = new Card(13, this.Content.Load<Texture2D>("AceDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[14] = new Card(14, this.Content.Load<Texture2D>("TwoDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[15] = new Card(15, this.Content.Load<Texture2D>("ThreeDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[16] = new Card(16, this.Content.Load<Texture2D>("FourDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[17] = new Card(17, this.Content.Load<Texture2D>("FiveDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[18] = new Card(18, this.Content.Load<Texture2D>("SixDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[19] = new Card(19, this.Content.Load<Texture2D>("SevenDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[20] = new Card(20, this.Content.Load<Texture2D>("EightDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[21] = new Card(21, this.Content.Load<Texture2D>("NineDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[22] = new Card(22, this.Content.Load<Texture2D>("TenDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[23] = new Card(23, this.Content.Load<Texture2D>("JackDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[24] = new Card(24, this.Content.Load<Texture2D>("QueenDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[25] = new Card(25, this.Content.Load<Texture2D>("KingDiamonds"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[26] = new Card(26, this.Content.Load<Texture2D>("AceHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[27] = new Card(27, this.Content.Load<Texture2D>("TwoHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[28] = new Card(28, this.Content.Load<Texture2D>("ThreeHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[29] = new Card(29, this.Content.Load<Texture2D>("FourHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[30] = new Card(30, this.Content.Load<Texture2D>("FiveHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[31] = new Card(31, this.Content.Load<Texture2D>("SixHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[32] = new Card(32, this.Content.Load<Texture2D>("SevenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[33] = new Card(33, this.Content.Load<Texture2D>("EightHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[34] = new Card(34, this.Content.Load<Texture2D>("NineHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[35] = new Card(35, this.Content.Load<Texture2D>("TenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[36] = new Card(36, this.Content.Load<Texture2D>("JackHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[37] = new Card(37, this.Content.Load<Texture2D>("QueenHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[38] = new Card(38, this.Content.Load<Texture2D>("KingHearts"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[39] = new Card(39, this.Content.Load<Texture2D>("AceSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[40] = new Card(40, this.Content.Load<Texture2D>("TwoSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[41] = new Card(41, this.Content.Load<Texture2D>("ThreeSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[42] = new Card(42, this.Content.Load<Texture2D>("FourSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[43] = new Card(43, this.Content.Load<Texture2D>("FiveSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[44] = new Card(44, this.Content.Load<Texture2D>("SixSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[45] = new Card(45, this.Content.Load<Texture2D>("SevenSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[46] = new Card(46, this.Content.Load<Texture2D>("EightSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[47] = new Card(47, this.Content.Load<Texture2D>("NineSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[48] = new Card(48, this.Content.Load<Texture2D>("TenSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[49] = new Card(49, this.Content.Load<Texture2D>("JackSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[50] = new Card(50, this.Content.Load<Texture2D>("QueenSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            cards[51] = new Card(51, this.Content.Load<Texture2D>("KingSpades"), this.Content.Load<Texture2D>("AceSpades"), new Vector2(100, 100), true);
+            #endregion Create cards[]
 
             for (int i = 0; i < cards.Length; i++)
             {
